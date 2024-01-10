17f07a136562cf01fe031516efbdf29dbc7373fe
Sheryl Chee <sherylmichaela@gmail.com>
2024-01-11 09:51:24 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+
+let writeCards = (name, occasion) => {
+  for (let i = 0; i < name.length; i++) {
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+};
+
+let countDown = () => {
+  let num = 10;
+  while (num >= 0) {
+    console.log(num);
+    num -= 1;
+  }
+};
